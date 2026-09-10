6784e8ab2ee31e3d8368ad57587432e49e98753e
ahmadshahdev <kingshah2003@gmail.com>
2026-09-10 16:37:16 +0500

    added routes in app.jsx added app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,47 @@
-import { useState } from 'react'
-import './App.css'
-import './Components/AboutUs'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import AboutUs from "./AboutUs";
+import "./App.css";
+
+const PlaceholderProducts = () => (
+	<div style={{ padding: "50px" }}>
+		Product Listing Page (Coming in Module 4)
+	</div>
+);
+const PlaceholderCart = () => (
+	<div style={{ padding: "50px" }}>
+		Shopping Cart Page (Coming in Module 5)
+	</div>
+);
+
+const LandingPage = () => {
+	return (
+		<div className="landing-page">
+			<div className="landing-content">
+				{/* Company Name */}
+				<h1>Paradise Nursery</h1>
+
+				{/* Paragraph about the company */}
+				<AboutUs />
+
+				{/* Get Started button linking to product page */}
+				<Link to="/products">
+					<button className="get-started-btn">Get Started</button>
+				</Link>
+			</div>
+		</div>
+	);
+};
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('landing');
-
-  return (
-    <>
-      
-    </>
-  )
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/products" element={<PlaceholderProducts />} />
+				<Route path="/cart" element={<PlaceholderCart />} />
+			</Routes>
+		</Router>
+	);
 }
-
-export default App
+export default App;
